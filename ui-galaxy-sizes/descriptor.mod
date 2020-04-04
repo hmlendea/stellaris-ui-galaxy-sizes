f3d8d31fdf71fd14f2e16d5afe21a7b144652d2b
Mlendea, Horațiu <Mlendea.Horatiu@GMail.com>
2020-04-04 14:10:11 +0300

    Moved the mod from hmlendea/stellaris-universum-infinitum

---
+++ ui-galaxy-sizes/descriptor.mod
@@ -0,0 +1,7 @@
+name="Universum Infinitum - Galaxy Sizes"
+path="mod/ui-galaxy-sizes"
+tags={
+    "Galaxy Generation"
+}
+picture="ui.jpg"
+supported_version="2.5.*"
---
+++ ui-galaxy-sizes/map/galaxy/base.lua
@@ -0,0 +1,14 @@
+core_radius_perc = 0.15
+num_stars_core_perc = 0
+stars_min_dist = 9.0
+
+countries = {
+    ideal_sq_dist_between = 5625,
+    min_sq_dist_between = 2500,
+}
+
+fallen_empires = {
+    ideal_sq_dist_between = 15625,
+    min_sq_dist_between = 5625
+}
+
